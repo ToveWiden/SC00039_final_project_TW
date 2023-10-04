6447441ff23065cef6e28623653adef1592e5679
Tove Widén <wtove@chalmers.se>
2023-10-04 16:42:28 +0200

    making fuctions

--- project_script.py
+++ project_script.py
@@ -5,12 +5,13 @@ import seaborn as sns
 import numpy as np
 
 ######### read the cazy file ########
-df = pd.read_table('cazy_data.txt', header=None) #make into pandas df
-df.columns = ['Family','Domain','Strain','Protein'] #Creates a header
-df.head(10)
+def read_cazy_file(filename,df):
+    df = pd.read_table(filename, header=None) #make into pandas df
+    df.columns = ['Family','Domain','Strain','Protein'] #Creates a header
+    return df
 
 ######### Remove duplicates that shouldn't be there #########
-#The file downloaded from cazy contains some redundancy. Because I want to look
+#The file downloaded from the cazy database contains some redundancy. Because I want to look
 #at occurences where there are several enzyme families connected to the same protein
 #the duplicate rows that should not be in the file needs to be removed. 
 #They cannot be removed simply by df.drop_duplicate() because that would also remove
@@ -18,27 +19,36 @@ df.head(10)
 #Correct duplicates are almost always consecutive. The could be non consecutive if 
 #there are several subfamilies but the same family in the same protein. Then some 
 #extra rows will be kept. This will be good enough for now. 
-df['consecutive_protein'] = (df.groupby(
-    df['Protein'].ne(df['Protein'].shift()).cumsum()
-).cumcount() + 1) #Counts the number of consecutive proteins in the rows and
-#writes it in a new column
 
-duplicate_free_df = df.drop_duplicates() #removes the duplicates based on 
-#all columns, correct duplicates (consecutive ones) will therefore be kept
-duplicate_free_df
-df_D = duplicate_free_df[['Family','Domain','Strain','Protein']]
-df_D.head()
-
+def remove_faulty_duplicates(df1,df2):
+    df1['consecutive_protein'] = (df1.groupby(df1['Protein'].ne(df1['Protein'].shift()).cumsum()).cumcount() + 1) #Counts the number of consecutive proteins in the rows and writes it in a new column
+    df1.drop_duplicates() #removes the duplicates based on all columns, correct duplicates (consecutive ones) will therefore be kept
+    df2 = df1
+    df2 = df2[['Family','Domain','Strain','Protein']]
 ################################################
 
 #filter if wanted
-noCBMs_df = df_D[~df_D['Family'].str.startswith('CBM')] #creates a dataframe without the entries where the family is a CBM
+def make_subset(df,df_subset, include_CBMs=True ,**Domain,**Strain):
+    if include_CBMs == False:
+        df1 = df[~df['Family'].str.startswith('CBM')] #creates a dataframe without the entries where the family is a CBM
+    else:
+        df1=df
+    if Domain = True:
+        df2 = df1[df1.Domain == Domain] #creates a df with the selected domain
+    else:
+        df2 = df1
+    if Strain = True:
+        df_subset = df2[df2.Strain == Strain]
+    else:
+        df_subset = df2
+    
+    
 df_subset_bacteria = noCBMs_df[noCBMs_df.Domain == 'Bacteria'] #creates a df with the selected domain
 df_subset_strain = noCBMs_df[noCBMs_df.Strain == 'Bacteroides thetaiotaomicron 7330'] #creates df with the selected strain
 #sort on protein
 #number entries per protein
 #
-a=noCBMs_df.sort_values('Protein') #sorts df based on protein name
+a=df_subset_noCBMs.sort_values('Protein') #sorts df based on protein name
 a['entries'] = (a.groupby(
     a['Protein'].ne(a['Protein'].shift()).cumsum()
 ).cumcount() + 1) #adds a column where each entry with the same protein gets a number
@@ -46,7 +56,7 @@ a['entries'] = (a.groupby(
 b=a.sort_values('entries')
 
 
-counts = b['Protein'].value_counts()
+counts = a['Protein'].value_counts()
 counts_df = pd.DataFrame(counts)
 counts_df = counts_df.reset_index(level=0)
 counts_df.columns = ['Protein', 'Number of Domains']
@@ -54,21 +64,15 @@ number_of_domains = counts_df['Number of Domains'].value_counts()
 number_of_domains_df = pd.DataFrame(number_of_domains)
 number_of_domains_df = number_of_domains_df.reset_index(level=0)
 number_of_domains_df.columns = ['Number of Domains','Count']
-number_of_multicats_df = number_of_domains_df[number_of_domains_df['Number of Domains']>1]
-number_of_multicats_df.head()
+number_of_domains_df_subset = number_of_domains_df[(number_of_domains_df['Number of Domains'] >=0) & (number_of_domains_df['Number of Domains'] <= float('inf'))]
 
 
 #plots a barplot how many proteins there are with 1 domains, 2 domains etc.
-g = sns.barplot(x=number_of_domains_df['Number of Domains'], y=number_of_domains_df['Count'])
+g = sns.barplot(x=number_of_domains_df_subset['Number of Domains'], y=number_of_domains_df['Count'])
 g.set_yscale("log")
 g.set(xlabel="Number of Domains", ylabel="Count")
 plt.show()
 
-#excludes single domains
-f = sns.barplot(x=number_of_multicats_df['Number of Domains'], y=number_of_multicats_df['Count'])
-f.set_yscale("log")
-f.set(xlabel="Number of Domains", ylabel="Count")
-plt.show()
 
 
 
